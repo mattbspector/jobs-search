ef31bf544cfe1c653e54a05c0e87d29e591cc761
Matthew Spector <matt.bspector@yahoo.com>
2019-12-09 15:08:58 -0800

    Updates

---
+++ precache-manifest.746c78b77a55c84f59fc28c58a167adf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5fdc9150650bc6d6e21c83bd737e56",
+    "url": "/harvest/index.html"
+  },
+  {
+    "revision": "7b2c53ccab4086e5d9f9",
+    "url": "/harvest/static/css/main.5120dfb8.chunk.css"
+  },
+  {
+    "revision": "fb8db83d945173424ac8",
+    "url": "/harvest/static/js/2.315d8cb5.chunk.js"
+  },
+  {
+    "revision": "7b2c53ccab4086e5d9f9",
+    "url": "/harvest/static/js/main.59649c97.chunk.js"
+  },
+  {
+    "revision": "2146fc6f957ff811f52d",
+    "url": "/harvest/static/js/runtime~main.2e3d3c23.js"
+  }
+]);
